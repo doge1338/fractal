6353f4729466fb7a68bf53c6d469c31ffcf68de3
kochampsy <74823830+kochampsy@users.noreply.github.com>
2020-11-22 16:06:48 +0100

    Performance and linear mixing for sampling

--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 	"log"
 	"math/rand"
 	"os"
-	"sync"
+	"runtime"
+	"runtime/pprof"
 	"time"
 )
 
@@ -22,11 +23,13 @@ const (
 	//size = 2.5
 
 	// Quality
-	imgWidth = 1024
-	maxIter  = 1000
-	samples  = 50
+	imgWidth     = 2048
+	maxIter      = 1000
+	samples      = 20
+	linearMixing = true
 
 	showProgress = true
+	profileCpu   = true
 )
 
 func main() {
@@ -53,62 +56,71 @@ func main() {
 }
 
 func render(img *image.RGBA) {
-	progress := make(chan struct{})
+	if profileCpu {
+		f, err := os.Create("profile.prof")
+		if err != nil {
+			panic(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
 
-	// Progress
-	if showProgress {
-		go func() {
-			for i := 1; ; i++ {
-				if _, k := <- progress; !k { break }
-				fmt.Printf("\r%d/%d (%d%%)", i, imgWidth, int(100*(float64(i) / float64(imgWidth))))
+	jobs := make(chan int)
+
+	for i := 0; i < runtime.NumCPU(); i++ {
+		go func () {
+			for y := range jobs {
+				for x := 0; x < imgWidth; x++ {
+					var r, g, b int
+					for i := 0; i < samples; i++ {
+						nx := size * ((float64(x) + rand.Float64()) / float64(imgWidth)) + px
+						ny := size * ((float64(y) + rand.Float64()) / float64(imgWidth)) + py
+						c := paint(mandelbrotIter(nx, ny, maxIter))
+						if linearMixing {
+							r += int(RGBToLinear(c.R))
+							g += int(RGBToLinear(c.G))
+							b += int(RGBToLinear(c.B))
+						} else {
+							r += int(c.R)
+							g += int(c.G)
+							b += int(c.B)
+						}
+					}
+					var cr, cg, cb uint8
+					if linearMixing {
+						cr = LinearToRGB(uint16(float64(r) / float64(samples)))
+						cg = LinearToRGB(uint16(float64(g) / float64(samples)))
+						cb = LinearToRGB(uint16(float64(b) / float64(samples)))
+					} else {
+						cr = uint8(float64(r) / float64(samples))
+						cg = uint8(float64(g) / float64(samples))
+						cb = uint8(float64(b) / float64(samples))
+					}
+					img.SetRGBA(x, y, color.RGBA{ R: cr, G: cg, B: cb, A: 255 })
+				}
 			}
-			fmt.Println()
 		}()
 	}
 
-	var wg sync.WaitGroup
 	for y := 0; y < imgWidth; y++ {
-		wg.Add(1)
-		go func(y int) {
-			for x := 0; x < imgWidth; x++ {
-				var sampledColours [samples]color.RGBA
-				for i := 0; i < samples; i++ {
-					nx := size * ((float64(x) + rand.Float64()) / float64(imgWidth)) + px
-					ny := size * ((float64(y) + rand.Float64()) / float64(imgWidth)) + py
-					sampledColours[i] = paint(mandelbrotIter(nx, ny, maxIter))
-				}
-				var r, g, b int
-				for _, colour := range sampledColours {
-					r += int(colour.R)
-					g += int(colour.G)
-					b += int(colour.B)
-				}
-				img.SetRGBA(x, y, color.RGBA{
-					R: uint8(float64(r) / float64(samples)),
-					G: uint8(float64(g) / float64(samples)),
-					B: uint8(float64(b) / float64(samples)),
-					A: 255,
-				})
-			}
-			if showProgress {
-				progress <- struct{}{}
-			}
-			wg.Done()
-		}(y)
+		jobs <- y
+		if showProgress {
+			fmt.Printf("\r%d/%d (%d%%)", y, imgWidth, int(100*(float64(y) / float64(imgWidth))))
+		}
 	}
-	wg.Wait()
-	close(progress)
+	if showProgress {
+		fmt.Printf("\r%d/%[1]d (100%%)\n", imgWidth)
+	}
 }
 
 func paint(r float64, n int) color.RGBA {
-	var insideSet = color.RGBA{ R: 255, G: 255, B: 255, A: 255 }
+	insideSet := color.RGBA{ R: 255, G: 255, B: 255, A: 255 }
 
 	if r > 4 {
-		c := hslToRGB(float64(n) / 800 * r, 1, 0.5)
-		return c
-	} else {
-		return insideSet
+		return hslToRGB(float64(n) / 800 * r, 1, 0.5)
 	}
+
+	return insideSet
 }
 
 func mandelbrotIter(px, py float64, maxIter int) (float64, int) {
