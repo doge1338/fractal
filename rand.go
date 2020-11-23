4df7284553287e02c04a1e8e2c3b9abc249afb0f
DeedleFake <deedlefake@users.noreply.github.com>
2020-11-23 15:08:48 -0500

    fix a minor data race

--- rand.go
+++ rand.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"sync/atomic"
 	"time"
 )
 
@@ -9,10 +10,12 @@ import (
 var randState = uint64(time.Now().UnixNano())
 
 func RandUint64() uint64 {
-	randState = ((randState ^ (randState << 13)) ^ (randState >> 7)) ^ (randState << 17)
-	return randState
+	oldState := atomic.LoadUint64(&randState)
+	newState := ((oldState ^ (oldState << 13)) ^ (oldState >> 7)) ^ (oldState << 17)
+	atomic.StoreUint64(&randState, newState)
+	return newState
 }
 
 func RandFloat64() float64 {
-	return float64(RandUint64() / 2) / (1 << 63)
+	return float64(RandUint64()/2) / (1 << 63)
 }
